5d9f29c828a5c46e8d72ea513f03517e1fc35973
unknown <shreeshubham23@gmail.com>
2024-01-01 20:37:33 +0530

    Create todo document API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
+const Todo = require('../src/models/todoModel');
 require('../src/db/conn');
 
 const port = process.env.PORT || 3000;
@@ -9,6 +11,18 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
+
+// Add todo document API
+app.post('/todos/', async (req, res) => {
+    try {
+        const todo = await Todo.create(req.body);
+        res.status(200).json(todo);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
+
 
 app.listen(port, () => {
     console.log(`Server Running at http://localhost:3000/`);
