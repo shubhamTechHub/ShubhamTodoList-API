b398166d2c8944e84377dcb9b70a9682dbf66448
unknown <shreeshubham23@gmail.com>
2024-01-01 22:48:14 +0530

    Add: Implement Delete Todo API functionality

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ app.put('/todos/:id', async (req, res) => {
         const { id } = req.params;
         const todo = await Todo.findByIdAndUpdate(id, req.body);
         if (!todo) {
-            res.status(404).json({message: `cannot find any product with ID ${id}`});
+            res.status(404).json({message: `cannot find any todo with ID ${id}`});
         }
         res.status(200).json(todo);
     } catch (error) {
@@ -61,6 +61,20 @@ app.put('/todos/:id', async (req, res) => {
     }
 });
 
+// delete todo - API
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await Todo.findByIdAndDelete(id);
+        if (!todo) {
+            res.status(404).json({message: `cannot find any todo with ID ${id}`});
+        }
+        res.status(200).json(todo);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server Running at http://localhost:3000/`);
